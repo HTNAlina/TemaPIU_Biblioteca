28b08e6f443050b4f1169eeb4ec1b115c96f8ceb
HTNAlina <92319150+HTNAlina@users.noreply.github.com>
2022-04-01 10:34:40 +0300

    Lab3

---
+++ Biblioteca/Biblioteca/Carte.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Carte
+    {
+        public string Nume { get; set; }
+        public string Autor { get; set; }
+        public string Editura { get; set; }
+        GENCARTE GenCarte;
+        int AnAparitie;
+        public int NrExemplare { get; set; }
+        int IDcarte;
+
+        //Constructor fara parametrii
+        public Carte()
+        {
+            Nume = string.Empty;
+            Autor = string.Empty;
+            Editura = string.Empty;
+            GenCarte = GENCARTE.Copii;
+            AnAparitie = 0;
+            NrExemplare = 0;
+            IDcarte = 0;
+        }
+        //Constructor cu parametrii
+        public Carte(string _nume, string _autor, string _editura, int _AnAparitie, int _NrExemplare, int _id, int n)
+        {
+            Nume = _nume;
+            Autor = _autor;
+            Editura = _editura;
+            AnAparitie = _AnAparitie;
+            NrExemplare = _NrExemplare;
+            IDcarte = _id;
+            GenCarte = (GENCARTE)(n);
+        }
+        //Constructor care primeste un sir unde elementele sunt despartite de ','
+        public Carte(string sirr)
+        {
+            string[] buff = sirr.Split(',');
+            Nume = buff[0];
+            Autor = buff[1];
+            Editura = buff[2];
+            AnAparitie = Convert.ToInt32(buff[3]);
+            NrExemplare = Convert.ToInt32(buff[4]);
+            IDcarte = Convert.ToInt32(buff[5]);
+            int i = Convert.ToInt32(buff[6]);
+            GenCarte = (GENCARTE)(i);
+        }
+
+        public string Info()
+        {
+            return string.Format("Cartea cu numele {0}, scrisa de {1}, publicata la editura {2}, a aparut in anul {3} si o avem disponibila in {4} exemplare ( are ID-ul : {5}).", Nume, Autor, Editura, AnAparitie, NrExemplare, IDcarte);
+        }
+    }
+}
---
+++ Biblioteca/Biblioteca/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Carte c1 = new Carte("Mandrie si Prejudecata", "Jane Austin", "Corint", 2014, 3, 132, 3);
+            string s1 = c1.Info();
+            Console.WriteLine(s1);
+
+            Carte c2 = new Carte("Marele Gatsby","F. Scott Fitzgerald","HUMANITAS",2018,2, 345 ,5);
+            string s2 = c2.Info();
+            Console.WriteLine(s2);
+
+            Persoana p1 = new Persoana("Hutan", "Alina", 22, 0, "0744", "alinahutan@email.com", 2);
+            string s3 = p1.InfoPersoana();
+            Console.WriteLine(s3);
+
+            Persoana p2 = new Persoana("Hutan", "Ioana", 20, 4, "09384","hutanioana@gmail.ro",2);
+            string s4 = p2.InfoPersoana();
+            Console.WriteLine(s4);
+
+            //CITIRE TASTATURA
+            Persoana p3 = CitirePersoanaTastatura();
+            Carte c3 = CitireCarteTastatura();
+
+            string s5 = p3.InfoPersoana();
+            Console.WriteLine(s5);
+
+            string s6 = c3.Info();
+            Console.WriteLine(s6);*/
+
+            IStocareData i = new IStocareData();
+            i.citireCartiFisier("C:\\Users\\blasc\\Desktop\\Biblioteca-Lab2\\Biblioteca\\carti.txt");
+            i.scriereFisierCarti("C:\\Users\\blasc\\Desktop\\Biblioteca-Lab2\\Biblioteca\\cartiOut.txt");
+            i.citirePersoaneFisier("C:\\Users\\blasc\\Desktop\\Biblioteca-Lab2\\Biblioteca\\persoane.txt");
+            i.scriereFisierPersoane("C:\\Users\\blasc\\Desktop\\Biblioteca-Lab2\\Biblioteca\\persoaneOut.txt");
+        }
+
+        public static Persoana CitirePersoanaTastatura()
+        {
+            Console.WriteLine("Numele persoanei: ");
+            string nume = Console.ReadLine();
+            Console.WriteLine("Prenumele persoanei: ");
+            string prenume = Console.ReadLine();
+            Console.WriteLine("Varsta persoanei: ");
+            int varsta = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Numar carti imprumutate: ");
+            int NrCartiImprumutate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Numarul de telefon: ");
+            string NrTelefon = Console.ReadLine();
+            Console.WriteLine("Adresa de mail: ");
+            string AdresaMail = Console.ReadLine();
+            Console.WriteLine("Statutul persoanei: ");
+            int st = Convert.ToInt32(Console.ReadLine());
+            Persoana p = new Persoana(nume, prenume, varsta, NrCartiImprumutate, NrTelefon, AdresaMail, st);
+            return p;
+        }
+
+        public static Carte CitireCarteTastatura()
+        {
+            Console.WriteLine("Numele cartii: ");
+            string nume = Console.ReadLine();
+            Console.WriteLine("Numele autorului: ");
+            string autor = Console.ReadLine();
+            Console.WriteLine("Numele editurii: ");
+            string editura = Console.ReadLine();
+            Console.WriteLine("An aparitie: ");
+            int AnAparitie = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Numar exemplare: ");
+            int NrExemplare = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Id carte: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Genul cartii: ");
+            int GenCarte = Convert.ToInt32(Console.ReadLine());
+            Carte c = new Carte(nume, autor, editura, AnAparitie, NrExemplare, id, GenCarte);
+            return c;
+        }
+    }
+}
